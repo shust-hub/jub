edea95a56721a76ea2839ae44c09e86ac40a581a
shust-hub <shut@mail2k.ru>
2023-07-07 16:08:40 +0300

    animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -385,20 +385,13 @@ jQuery(function ($) {
 		// Анимация блоков
 		new WOW().init();
 
-
-
-		// function myFunction(x) {
-		// 	if (x.matches) { // If media query matches
-		// 	  document.body.style.backgroundColor = "yellow";
-		// 	} else {
-		// 	  document.body.style.backgroundColor = "pink";
-		// 	}
-		//   }
-		  
-		//   var x = window.matchMedia("(max-width: 700px)")
-		//   myFunction(x) // Call listener function at run time
-		//   x.addListener(myFunction) // Attach listener function on state chang
-
+		let way = 100, // кол-во пройденных пикселей
+		deg = 10;  // угол поворота за пройденный путь
+		
+		$(window).on('scroll', function(){
+			let s = $(this).scrollTop();
+			$('.scroll-image').css('transform', 'rotate('+(s / way * deg)+'deg)');
+		});
 
 		// Копирование промокода
 		var cpnBtn = document.getElementById("cpnBtn");
